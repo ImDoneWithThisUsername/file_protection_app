f6eb61a80a51b0d54affeb00b53d4f8393a8d46a
duylunlxag <duylunlxag@gmail.com>
2022-12-29 23:42:23 +0700

    Yêu cầu 6
    Tách code thành main.py và main_usb.py. Để main_usb vào USB (volume letter F), mỗi lần chạy cần cắm usb mới hoàn chỉnh.

---
+++ main_usb.py
@@ -0,0 +1,91 @@
+import os
+from Crypto.Cipher import AES
+from Crypto.Hash import SHA256
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes, random
+
+class DataEncryption:
+    key_size = 32
+    iv_size = 16
+
+    def __init__(self, key: bytes) -> None:
+        self.key = key
+
+    def addSalt(self, data: bytes) -> bytes:
+        salt = get_random_bytes(random.randint(1,10)* 8)
+        data = salt + ".".encode("utf-8") + data
+        return data
+
+    def splitSalt(self, data:bytes) -> bytes:
+        pt_offset = data.find(".".encode("utf-8"))
+        pt = data[pt_offset + 1:]
+        return pt
+
+    def compareHashed(self, hashed:bytes) -> bool:
+        hash_object = SHA256.new(self.key)
+        if (hash_object.digest() == hashed):
+            return 1
+        return 0
+
+    def encrypt(self, data: bytes):
+        """
+        :return: 32 bytes hashed key, 24 bytes iv, cipher
+        """
+        hash_object = SHA256.new(self.key)
+        cipher = AES.new(pad(self.key, 16), AES.MODE_CBC)
+
+        data = self.addSalt(data)
+        ct_bytes = cipher.encrypt(pad(data, AES.block_size))
+
+        return hash_object.digest(), cipher.iv, ct_bytes
+        
+    def decrypt(self, data: bytes, iv: bytes) -> bytes:
+        try:
+            cipher = AES.new(pad(self.key, 16), AES.MODE_CBC, iv)
+            pt = unpad(cipher.decrypt(data), AES.block_size)
+
+            pt = self.splitSalt(pt)
+            return pt
+        except (ValueError, KeyError):
+            return None
+
+class FileEncryption:
+    def encrytion(self, path: str, key: str, path1: str, path2: str):
+        filename = os.path.basename(path)
+        file1_path = os.path.join(path1,filename+"1")
+        file2_path = os.path.join(path2,filename+"2")
+        with open(path,"rb") as file, open(file1_path,"wb") as f_out_1, open(file2_path,"wb") as f_out_2:
+            pt = file.read()
+            enc_object = DataEncryption(key.encode("utf-8"))
+
+            hashed_key, iv, ct = enc_object.encrypt(pt)
+            hashed_key = bytearray(hashed_key)
+            iv = bytearray(iv)
+            ct = bytearray(ct)
+
+            data1 = hashed_key[:16] + iv[:8] + ct[:int(len(ct)/2)]
+            data2 = hashed_key[16:] + iv[8:] + ct[int(len(ct)/2):]
+            f_out_1.write(data1)
+            f_out_2.write(data2)
+        file.close()
+        f_out_1.close()
+        f_out_2.close()
+
+    def decryption(self, filename1: str, filename2: str, key: str, filename_out:str):
+        with open(filename1,"rb") as file_in_1, open(filename2, "rb") as file_in_2:
+            data1 = file_in_1.read()
+            data2 = file_in_2.read()
+            enc_object = DataEncryption(key.encode("utf-8"))
+
+            hashed_key = data1[:16] + data2[:16]
+            iv = data1[16:16+8] + data2[16:16+8]
+            ct = data1[16+8:] + data2[16+8:]
+
+            if enc_object.compareHashed(hashed_key):
+
+                pt = enc_object.decrypt(ct, iv)
+                with open(filename_out, "wb") as f_out:
+                    f_out.write(pt)
+                f_out.close()
+        file_in_1.close()
+        file_in_2.close()
